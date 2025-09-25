7f27b397b930af742ab18425b9bcff08365d111d
DELAGREEN <delagreen@mail.ru>
2025-09-25 20:59:14 +0300

    init hw01

---
+++ hw01/Main.java
@@ -0,0 +1,42 @@
+package hw01;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main { // Добавлен класс
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        //Ввод размера массива
+        System.out.println("Введите размер массива: ");
+        int arraySize = scanner.nextInt();
+
+        if(arraySize<=0){
+            System.out.println("Размер массива не должен быть меньше 0");
+            scanner.close();
+            return;
+        }
+        
+        //Выбор типа массива
+        System.out.println("Выберите тип массива (1 - целочисленный, 2 - дробный): ");
+        int typeChoice = scanner.nextInt();
+
+        //Ввод границ генерации
+        System.out.println("Введите нижнюю границу генерации миссива: ");
+        double lowerBound = scanner.nextDouble();
+        System.out.println("Введите верхнюю границу генерации массива: ");
+        double upperBound = scanner.nextDouble();
+
+        if (lowerBound >= upperBound) {
+            System.out.println("Ошибка: нижняя граница должна быть меньше верхней");
+            scanner.close();
+            return;
+        }
+
+        if (typeChoice == 1) {
+            int[] intArray = new int[arraySize]
+            
+        }
+
+
+    }
+}
