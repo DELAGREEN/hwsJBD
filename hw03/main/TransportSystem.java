40deedfebbda3e6b290e6e10aeda42a818fadfd5
DELAGREEN <60788008+DELAGREEN@users.noreply.github.com>
2025-10-30 21:53:28 +0300

    Revert "#1"

--- hw03/main/TransportSystem.java
+++ hw03/main/TransportSystem.java
@@ -1,10 +1,5 @@
 package hw03.main;
 
-import hw03.main.ui.TransportUI;
-
 public class TransportSystem {
-    public static void main(String[] args) {
-        TransportUI ui = new TransportUI();
-        ui.start();
-    }
+    
 }
